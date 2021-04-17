164c3711d7fdce94a16218737d2b7099547959dc
George <60888284+ZavGeorge@users.noreply.github.com>
2021-04-17 14:36:00 +0300

    Add files via upload
    lab_5

---
+++ Graph.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms_lab_5
+{
+    class Graph
+    {
+
+        List<Vertex> V = new List<Vertex>();
+        List<Edge> E = new List<Edge>();
+
+        public int VertexCount => V.Count;
+        public int EdgeCount => E.Count;
+
+        public void AddVertex(Vertex vertex) 
+        {
+            V.Add(vertex);
+        }
+
+        public void AddEdge(Vertex from, Vertex to)
+        {
+            var edge = new Edge(from, to);
+            E.Add(edge);
+        }
+
+        public int[,] GetMatrix()
+        {
+            var matrix = new int[V.Count, V.Count];
+
+            foreach(var edge in E)
+            {
+                var row = edge.From.Number -1 ;
+                var column = edge.To.Number - 1;
+
+                matrix[row, column] = edge.Weight;
+            }
+
+            return matrix;
+        }
+
+
+    }
+}
