3984ed4c88e0dd886c2f1741949b215daff8fde8
George <60888284+ZavGeorge@users.noreply.github.com>
2021-05-27 01:32:49 +0300

    Add files via upload

---
+++ DoubleConnectedList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms_lab_7
+{
+    public class DoubleConnectedList<T>: IEnumerable
+    {
+        public DoubleConnected<T> Head { get; set; }
+        public DoubleConnected<T> Tail { get; set; }
+        public int Count { get; set; }
+        public DoubleConnectedList()
+        {
+
+        }
+
+        public DoubleConnectedList(T data)
+        {
+            var item = new DoubleConnected<T>(data);
+            Head = item;
+            Tail = item;
+            Count = 1;
+        }
+
+        public void Add(T data)
+        {
+            var item = new DoubleConnected<T>(data);
+
+            if (Count == 0)
+            {
+                Head = item;
+                Tail = item;
+                Count = 1;
+            }
+
+            Tail.Next = item;
+            item.Previous = Tail;
+            Tail = item;
+            Count++;
+        }
+        public void Delete(T data)
+        {
+
+            var current = Head;
+            while(current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    current.Previous.Next = current.Next;
+                    current.Next.Previous = current.Previous;
+                    Count--;
+                    return;
+                }
+                current = current.Next;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var current = Head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Algorithms_lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Реализовать средствами ООП списки посредством массивов и с помощью указателей.Выполнить пример списка.");
+            var list = new LinkedList<int>();
+            list.Add(12);
+            list.Add(32);
+            list.Add(65);
+            list.Add(234);
+            list.Add(6);
+            list.Delete(12);
+            foreach(var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Реализовать средствами ООП двусвязные списки. Реализовать списки в виде класса.");
+            var doublelist = new DoubleConnectedList<int>();
+            doublelist.Add(1);
+            doublelist.Add(3);
+            doublelist.Add(6);
+            doublelist.Add(-3);
+            doublelist.Add(43);
+            doublelist.Delete(1);
+            foreach(var item in doublelist)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Реализовать средствами ООП кольцевые списки.");
+            var circularlist = new CircularLinkedList<int>();
+            circularlist.Add(1);
+            circularlist.Add(-32);
+            circularlist.Add(43);
+            circularlist.Add(54);
+            circularlist.Add(-5);
+            circularlist.Add(0);
+            circularlist.Delete(1);
+            foreach(var item in circularlist)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Реализовать средствами ООП ограниченную очередь в виде массива.");
+            var arrayqueue = new ArrayQueue<int>(5);
+            arrayqueue.Enqueue(1);
+            arrayqueue.Enqueue(2);
+            arrayqueue.Enqueue(4);
+            arrayqueue.Enqueue(5);
+            arrayqueue.Enqueue(9);
+            Console.Write(arrayqueue.Dequeue() + " ");
+            Console.Write(arrayqueue.Dequeue() + " ");
+            Console.Write(arrayqueue.Dequeue() + " ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Реализовать средствами ООП ограниченный стек в виде массива");
+            var arraystack = new ArrayStack<int>(5);
+            arraystack.Push(21);
+            arraystack.Push(12);
+            arraystack.Push(1);
+            arraystack.Push(3);
+            arraystack.Push(4);
+            Console.Write(arraystack.Pop() + " ");
+            Console.Write(arraystack.Pop() + " ");
+            Console.Write(arraystack.Pop() + " ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Реализовать средствами ООП стек в виде связного списка");
+            var doubleconnectedstack = new DoubleConnectedStack<int>();
+            doubleconnectedstack.Push(12);
+            doubleconnectedstack.Push(16);
+            doubleconnectedstack.Push(-32);
+            doubleconnectedstack.Push(0);
+            Console.Write(doubleconnectedstack.Pop() + " ");
+            Console.Write(doubleconnectedstack.Pop() + " ");
+            Console.Write(doubleconnectedstack.Pop() + " ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Реализовать средствами ООП с помощью стека вычисление постфиксных выражений.Вычислить пример: 2 * (3 + 2) * ((5 * 2 - 8) + (6 - 2 * 2))");
+            var example = new PostfixExpressions();     
+            Console.Write("Введите выражение: ");
+            Console.WriteLine(example.Calculate(Console.ReadLine()));
+           
+
+
+
+        }
+    }
+}
