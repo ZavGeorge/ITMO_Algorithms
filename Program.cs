9585fa46b3ff974481be068245ad618632d96881
George <60888284+ZavGeorge@users.noreply.github.com>
2021-06-05 23:24:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algorithms_lab_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dophantine = new CDiophantine(25);
+            for(int i =0; i < 15; i++)
+            {
+                dophantine.Selection(0.6);
+                foreach(var item in dophantine.population)
+                {
+                    if (item.Fitness() == 0)
+                    {
+                        Console.WriteLine(item.ToString());
+                    }
+                }
+            }
+
+
+            
+        }
+    }
+}
